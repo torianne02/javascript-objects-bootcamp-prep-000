27eaac6afd0cb8e0f69d5632f6cfb20fd3f93a40
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-03-15 22:10:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,8 @@ var playlist = {
   artistName: 'songTitle'
 };
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle;
+  playlist['artistName'] = songTitle;
+  return playlist;
 }
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName;
